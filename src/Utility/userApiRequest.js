e6a09c7f1aa9b68f563421af2613380c6940bf78
Fagner Kenedy <fagnerkenedy@hotmail.com>
2024-05-03 16:42:06 -0300

    ajuste

--- src/Utility/userApiRequest.js
+++ src/Utility/userApiRequest.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 const token = process.env.REACT_APP_USER_API_TOKEN
+console.log("token", token)
 function userApiRequest(config) {
   return new Promise((resolve, reject) => {
     axios({
