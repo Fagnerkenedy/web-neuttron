383d8120780bb894354ecd6c69bd813b0dd53cf6
Fagner Kenedy <fagnerkenedy@hotmail.com>
2024-06-01 23:20:17 -0300

    Adiciona permissão nos itens das configurações

--- src/components/content/settings/Settings.js
+++ src/components/content/settings/Settings.js
@@ -3,30 +3,43 @@ import { Layout, Card, Row } from 'antd';
 import Link from "antd/es/typography/Link";
 import '../styles.css'
 import { Content } from "antd/es/layout/layout";
+import { Can } from "../../../contexts/AbilityContext.js";
+import { useAbility } from '../../../contexts/AbilityContext.js'
 
 function Settings() {
+    const { ability, loading } = useAbility();
     const currentPath = window.location.pathname;
     const pathParts = currentPath.split('/');
     const org = pathParts[1]
     return (
         <Content className='content' style={{ paddingTop: '20px' }}>
             <Layout style={{ minHeight: 'calc(100vh - 160px)' }}>
-                <Card style={{paddingLeft: '15px'}} title="Configurações">
-                    <Row style={{ marginBottom: '20px'}}>
-                        <Link href={`/${org}/settings/modules`}>Módulos e campos</Link>
-                    </Row>
-                    <Row style={{ marginBottom: '20px'}}>
-                        <Link href={`/${org}/users`}>Usuários</Link>
-                    </Row>
-                    <Row style={{ marginBottom: '20px'}}>
-                        <Link href={`/${org}/profiles`}>Perfis</Link>
-                    </Row>
-                    <Row style={{ marginBottom: '20px'}}>
-                        <Link href={`/${org}/functions`}>Funções</Link>
-                    </Row>
-                    <Row>
-                        <Link href={`/${org}/charts`}>Painéis</Link>
-                    </Row>
+                <Card style={{ paddingLeft: '15px' }} title="Configurações">
+                    <Can I='read' a={'modules'} ability={ability}>
+                        <Row style={{ marginBottom: '20px' }}>
+                            <Link href={`/${org}/settings/modules`}>Módulos e campos</Link>
+                        </Row>
+                    </Can>
+                    <Can I='read' a={'users'} ability={ability}>
+                        <Row style={{ marginBottom: '20px' }}>
+                            <Link href={`/${org}/users`}>Usuários</Link>
+                        </Row>
+                    </Can>
+                    <Can I='read' a={'profiles'} ability={ability}>
+                        <Row style={{ marginBottom: '20px' }}>
+                            <Link href={`/${org}/profiles`}>Perfis</Link>
+                        </Row>
+                    </Can>
+                    <Can I='read' a={'functions'} ability={ability}>
+                        <Row style={{ marginBottom: '20px' }}>
+                            <Link href={`/${org}/functions`}>Funções</Link>
+                        </Row>
+                    </Can>
+                    <Can I='read' a={'charts'} ability={ability}>
+                        <Row>
+                            <Link href={`/${org}/charts`}>Painéis</Link>
+                        </Row>
+                    </Can>
                 </Card>
             </Layout>
         </Content>
