c122785dadf0f6612ef4ea0d8a3787f31717d7dc
Fagnerkenedy <fagnerkenedy@hotmail.com>
2024-10-31 00:38:43 -0300

    Adiciona dropdown para quando tiver muitos módulos

---
+++ src/components/header/CustomDropdown.js
@@ -0,0 +1,126 @@
+import React, { useRef, useState } from 'react';
+import { Dropdown, Input, Button, Menu, Space, Divider, theme } from 'antd';
+import { EllipsisOutlined, PlusOutlined } from '@ant-design/icons';
+import Link from 'antd/es/typography/Link';
+import { Can } from '../../contexts/AbilityContext';
+const { useToken } = theme;
+
+const CustomDropdown = ({ extraModules, org, setActiveModule, darkMode, ability }) => {
+    const [searchValue, setSearchValue] = useState('');
+    const ref = useRef(null);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleModuleChange = (moduleName) => {
+        setIsOpen(false);
+        window.location.href = `/${org}/${moduleName}`
+    };
+
+    const openSelect = () => {
+        setIsOpen(!isOpen);
+        setTimeout(() => {
+            if (ref.current) {
+                ref.current.focus({
+                    cursor: 'all',
+                });
+            }
+        }, 100);
+    };
+
+    const handleSearchChange = (e) => {
+        setSearchValue(e.target.value);
+    };
+
+    const handleModuleSelect = (module) => {
+        setActiveModule(module.name);
+        setIsOpen(false); // Fecha o dropdown após a seleção
+        // Navega para o link do módulo
+        window.location.href = `/${org}/${module.api_name || module.name}`;
+    };
+
+    const filteredModules = extraModules.filter((module) =>
+        module.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+
+    const menu = (
+        <Menu>
+            <Menu.Item>
+                <Input
+                    ref={ref}
+                    placeholder="Pesquisar módulos"
+                    value={searchValue}
+                    onChange={handleSearchChange}
+                    // onBlur={() => setIsOpen(false)} // Fecha ao perder o foco
+                    autoFocus
+                />
+            </Menu.Item>
+            {filteredModules.length > 0 ? (
+                filteredModules.map((module) => (
+
+                    // <Can I='read' a={(module.api_name ? module.api_name : module.name)} ability={ability}>
+                        <Menu.Item
+                            key={module.name}
+                            onClick={() => handleModuleSelect(module)}
+                            style={{
+                                padding: '8px 16px',
+                                cursor: 'pointer',
+                            }}
+                        >
+                            <span style={{ color: darkMode ? '#fff' : '#000', display: 'block', width: '100%' }}>
+                                {module.name.charAt(0).toUpperCase() + module.name.slice(1)}
+                            </span>
+                        </Menu.Item>
+                    // </Can> 
+                ))
+            ) : (
+                <Menu.Item disabled>Nenhum módulo encontrado</Menu.Item>
+            )}
+        </Menu>
+    );
+
+    const { token } = useToken();
+    const contentStyle = {
+        backgroundColor: token.colorBgElevated,
+        borderRadius: token.borderRadiusLG,
+        boxShadow: token.boxShadowSecondary
+    };
+
+    const menuStyle = {
+        boxShadow: 'none',
+    };
+
+    return (
+        <div>
+            <Dropdown
+                overlay={menu}
+                trigger={['click']}
+                onVisibleChange={(visible) => setIsOpen(visible)}
+                visible={isOpen}
+                dropdownRender={(menu) => (
+                    <div style={contentStyle}>
+                        {React.cloneElement(menu, {
+                            style: menuStyle,
+                        })}
+                        <Divider
+                            style={{
+                                margin: 0,
+                            }}
+                        />
+                        <Space
+                            style={{
+                                padding: 4,
+                                display: 'flex',
+                                justifyContent: 'center'
+                            }}
+                        >
+                            <Button type='link' href={`/${org}/settings/modules`}>Criar Novo Módulo</Button>
+                        </Space>
+                    </div>
+                )}
+            >
+                <Button type='text' onClick={openSelect} icon={<EllipsisOutlined />} />
+            </Dropdown>
+        </div>
+    );
+};
+
+export default CustomDropdown;
--- src/components/header/Modules.js
+++ src/components/header/Modules.js
@@ -1,8 +1,8 @@
 import { useState, useEffect, useRef } from 'react';
 import Link from 'antd/es/typography/Link';
-import { Menu, Dropdown, Button, Col } from 'antd';
-import { EllipsisOutlined } from '@ant-design/icons';
+import { Col } from 'antd';
 import { Can } from '../../contexts/AbilityContext';
+import CustomDropdown from './CustomDropdown';
 
 const HeaderModules = ({ modules, org, darkMode, activeModule, setActiveModule, ability }) => {
     const ref2 = useRef(null);
@@ -15,64 +15,47 @@ const HeaderModules = ({ modules, org, darkMode, activeModule, setActiveModule, 
             let currentWidth = 0;
             const visible = [];
             const extra = [];
-          
+
             // Primeiro, verifica e inclui o módulo ativo na lista de visíveis, se ele for encontrado
             const activeModuleObj = modules.find(module => module.name === activeModule || module.api_name === activeModule);
             const activeModuleWidth = activeModuleObj ? activeModuleObj.name.length * 8 + 50 : 0;
-          
+
             // Verifica o tamanho disponível e adiciona os módulos na sequência
             modules.forEach((module) => {
-              const moduleWidth = module.name.length * 8 + 50;
-          
-              if (module === activeModuleObj) {
-                // Se o módulo ativo precisa substituir módulos visíveis
-                while (currentWidth + activeModuleWidth > containerWidth && visible.length > 0) {
-                  const removedModule = visible.pop(); // Remove o último módulo visível
-                  currentWidth -= removedModule.name.length * 8 + 50;
-                  extra.unshift(removedModule); // Adiciona o módulo removido ao início dos extras
-                }
-          
-                if (currentWidth + activeModuleWidth <= containerWidth) {
-                  visible.push(activeModuleObj); // Adiciona o módulo ativo no final dos visíveis
-                  currentWidth += activeModuleWidth;
+                const moduleWidth = module.name.length * 8 + 50;
+
+                if (module === activeModuleObj) {
+                    // Se o módulo ativo precisa substituir módulos visíveis
+                    while (currentWidth + activeModuleWidth > containerWidth && visible.length > 0) {
+                        const removedModule = visible.pop(); // Remove o último módulo visível
+                        currentWidth -= removedModule.name.length * 8 + 50;
+                        extra.unshift(removedModule); // Adiciona o módulo removido ao início dos extras
+                    }
+
+                    if (currentWidth + activeModuleWidth <= containerWidth) {
+                        visible.push(activeModuleObj); // Adiciona o módulo ativo no final dos visíveis
+                        currentWidth += activeModuleWidth;
+                    } else {
+                        extra.push(activeModuleObj); // Se ainda não couber, vai para o dropdown
+                    }
+                } else if (currentWidth + moduleWidth < containerWidth) {
+                    visible.push(module);
+                    currentWidth += moduleWidth;
                 } else {
-                  extra.push(activeModuleObj); // Se ainda não couber, vai para o dropdown
+                    extra.push(module);
                 }
-              } else if (currentWidth + moduleWidth < containerWidth) {
-                visible.push(module);
-                currentWidth += moduleWidth;
-              } else {
-                extra.push(module);
-              }
             });
-          
+
             setVisibleModules(visible);
             setExtraModules(extra);
-          };
-          
+        };
+
 
         checkOverflow();
         window.addEventListener('resize', checkOverflow);
 
         return () => window.removeEventListener('resize', checkOverflow);
     }, [modules, activeModule]);
-
-    const extraModulesMenu = (
-        <Menu>
-            {extraModules.map((module, index) => (
-                <Menu.Item key={index}>
-                    <Link
-                        className={`modules ${activeModule === (module.api_name ? module.api_name : module.name) ? 'active' : ''}`}
-                        style={{ color: darkMode ? '#fff' : '#000' }}
-                        href={`/${org}/${(module.api_name ? module.api_name : module.name)}`}
-                        onClick={() => setActiveModule(module.name)}
-                    >
-                        {module.name.charAt(0).toUpperCase() + module.name.slice(1)}
-                    </Link>
-                </Menu.Item>
-            ))}
-        </Menu>
-    );
 
     return (
         <Col span={16} ref={ref2} style={{ margin: "20px", display: 'flex', alignItems: 'center', flexWrap: 'nowrap' }}>
@@ -98,11 +81,7 @@ const HeaderModules = ({ modules, org, darkMode, activeModule, setActiveModule, 
                 </Can>
             ))}
 
-            {extraModules.length > 0 && (
-                <Dropdown overlay={extraModulesMenu} trigger={['hover']}>
-                    <Button icon={<EllipsisOutlined />} />
-                </Dropdown>
-            )}
+            <CustomDropdown extraModules={extraModules} org={org} setActiveModule={setActiveModule} darkMode={darkMode} ability={ability} />
         </Col>
     );
 };
