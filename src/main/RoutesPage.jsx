3a5962c36e2150785f3a9b3fb8126ec8cfca87ac
Fagnerkenedy <fagnerkenedy@hotmail.com>
2025-01-18 22:16:29 -0300

    Adiciona chat em tempo real

--- src/main/RoutesPage.jsx
+++ src/main/RoutesPage.jsx
@@ -25,7 +25,7 @@ import NewPassword from '../components/users/NewPassword.jsx';
 import { io } from 'socket.io-client';
 // import PermissionsPage from '../components/content/detailView/PermissionsPage.js';
 
-const socket = io('https://crm.neuttron.com.br:3003');
+const socket = io(process.env.REACT_APP_LINK_API);
 
 function RoutesPage() {
   const currentPath = window.location.pathname;
