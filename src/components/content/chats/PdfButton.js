5f22d408b4fef809156c533408342767d6b9b65c
Fagnerkenedy <fagnerkenedy@hotmail.com>
2025-02-09 23:55:41 -0300

    Enviado de forma automatica.

--- src/components/content/chats/PdfButton.js
+++ src/components/content/chats/PdfButton.js
@@ -1,14 +1,15 @@
 import { Button } from "antd";
 import { FilePdfOutlined } from "@ant-design/icons";
+import TextoFormatado from "./TextoFormatado";
 
 const PdfDownload = ({ pdfUrl, fileName }) => (
-  <div style={{ padding: "10px", display: "flex", alignItems: "center", gap: "10px" }}>
-    <FilePdfOutlined style={{ fontSize: "24px", color: "red" }} />
-    <span>{fileName}</span>
-    <Button type="link" href={pdfUrl} download target="_blank">
-      Baixar
-    </Button>
-  </div>
+    <div style={{ padding: "10px", display: "flex", alignItems: "center", gap: "10px" }}>
+        <FilePdfOutlined style={{ fontSize: "24px", color: "red" }} />
+        <TextoFormatado fileName={fileName} />
+        <Button type="link" href={pdfUrl} download target="_blank">
+            Baixar
+        </Button>
+    </div>
 );
 
 export default PdfDownload;
