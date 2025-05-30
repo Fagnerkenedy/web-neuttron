91ef48bcab8b7c455358ecc719283c1db5dc1640
Fagnerkenedy <fagnerkenedy@hotmail.com>
2025-05-30 09:16:11 -0300

    Enviado de forma automatica.

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,13 @@ git commit -m "Enviado de forma automatica."
 echo "Pushing to git.."
 git push
 
-# echo "Deploying files to server..."
-# scp -r build/* ubuntu@crm.neuttron.com.br:/var/www/52.67.55.72
+echo "Deploying files to server..."
+scp -i "C:/Users/fagne/.ssh/neuttron-server.pem" -r dist/* ubuntu@crm.neuttron.com.br:/home/ubuntu/temp_deploy/
+
+echo "Accessing server"
+ssh -i "C:/Users/fagne/.ssh/neuttron-server.pem" ubuntu@crm.neuttron.com.br
+
+echo "Moving to web folder"
+sudo mv /home/ubuntu/temp_deploy/* /var/www/52.67.55.72/
+
+echo "✅ Deploy finalizado com sucesso!"
